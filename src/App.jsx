e0af604a618edb05c9249bb2103d942894ed71aa
samuel vanegas quintero <samuvanegasq23@gmail.com>
2026-07-22 16:28:33 -0500

    se esta creando un proyecto

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA </h1> 
+ <p>soy estudiante del sena tengo 18 años y en este momento estoy haciendo un tecnologo en ADSO</p>
+</main>
+ ) } 
