cfc964e4fd9f83ffeb14fbd1c7a2e88bc606b1c2
miguelmonsalve29xd <miguelmonsalve29xd@gmail.com>
2026-08-19 17:03:02 -0500

    Clase_6_Tailwind_UI_Agenda_ADSO_v3

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,15 +1,51 @@
-export default function ContactoCard({ nombre, telefono, correo, etiqueta, onEliminar }) {
-  return (
-    <article className="tarjeta-contacto">
-      <h3>{nombre}</h3>
-      <p>📞 {telefono}</p>
-      <p>✉️ {correo}</p>
-      {etiqueta && <p>{etiqueta}</p>}
-      <div className="acciones">
-        <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
-          Eliminar
-        </button>
-      </div>
-    </article>
-  );
+// src/components/ContactoCard.jsx
+export default function ContactoCard({
+ nombre,
+ telefono,
+ correo,
+ etiqueta,
+ onEliminar
+}) {
+ return (
+ <article className="bg-white border border-gray-200 rounded-xl shadow-sm p-5">
+ {/* Nombre destacado */}
+ <h3 className="text-xl font-bold text-gray-900 mb-3">
+ {nombre}
+
+ </h3>
+ {/* Teléfono */}
+ <p className="text-gray-700 mb-1"></p>
+ <h3 className="text-xl font-bold text-gray-900 mb-3">
+ {telefono}
+
+
+ </h3>
+
+{/* correo */}
+ <p className="text-gray-700 mb-1"></p>
+ <h3 className="text-xl font-bold text-gray-900 mb-3">
+ {correo}
+
+ </h3>
+
+ {/* Etiqueta */}
+ <p className="text-gray-700 mb-1"></p>
+ <h3 className="text-xl font-bold text-gray-900 mb-3">
+ {etiqueta}
+
+ </h3>
+ 
+
+ {/* Acciones (alineado a la izquierda en móvil) */}
+ <div className="flex justify-start">
+ <button
+ onClick={() => onEliminar(correo)}
+ className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium
+px-4 py-2 rounded-lg transition-colors"
+ >
+ Eliminar
+ </button>
+ </div>
+ </article>
+ );
 }
